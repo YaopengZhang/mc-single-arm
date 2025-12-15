024637c4eaa9d33014f609c2812423c22fd016dc
YaopengZhang <1066944636@qq.com>
2025-12-15 02:11:48 -0500

    Updated KinC_list and the relevant infiles

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,8 @@
+module purge
+module use /group/halla/modulefiles
+module use /group/nps/modulefiles
+module load nps_replay/5.28.24
+
 setenv ROOTSYS /cvmfs/oasis.opensciencegrid.org/jlab/scicomp/sw/el9/root/6.30.04-gcc11.4.0
 setenv LD_LIBRARY_PATH ${ROOTSYS}/lib:${LD_LIBRARY_PATH}
 setenv PATH ${ROOTSYS}/bin:${PATH}
